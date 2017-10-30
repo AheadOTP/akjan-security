ab785c776a1aec9852fadeda9e9ed992921bd01b
Aybek Kembayev <aheadofthepack15@gmail.com>
2017-10-30 22:32:58 +0600

    Navbar scaffold

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
 import { Grid, Navbar } from 'react-bootstrap';
 import BsCarousel from './components/BsCarousel';
+import BsNavbar from './components/BsNavbar';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
+      <div>
+        <BsNavbar />
         <BsCarousel />
+      </div>
     );
   }
 }
