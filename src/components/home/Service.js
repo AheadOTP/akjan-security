74e5c047d75aa3f3cc2b03a27c11554ce774c3e7
Ibek Kem <aheadofthepack15@gmail.com>
2017-12-18 13:57:17 +0600

    Showing service images added proxy in package json

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -2,12 +2,11 @@ import React from 'react';
 import '../../styles/Service.css';
 
 function Service(props) {
-  const imgUrl = props.imgUrl;
-  console.log(imgUrl);
   return (
     <div className="container">
       <h2>{props.header}</h2>
       <p>{props.text}</p>
+      <img alt="" src={props.imgUrl} />
     </div>
   );
 }
