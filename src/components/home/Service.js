c11a931f01798debcceb6af8417915097a78c59d
Ibek Kem <aheadofthepack15@gmail.com>
2017-12-19 13:18:10 +0600

    changed order of even elemnts

--- src/components/home/Service.js
+++ src/components/home/Service.js
@@ -4,9 +4,11 @@ import '../../styles/Service.css';
 function Service(props) {
   return (
     <div className="container">
-      <h2>{props.header}</h2>
-      <p>{props.text}</p>
-      <img alt="" src={props.imgUrl} />
+      <div className="serviceDescription serviceElement">
+        <h2>{props.header}</h2>
+        <p>{props.text}</p>
+      </div>
+      <img className="serviceImage serviceElement" alt="" src={props.imgUrl} />
     </div>
   );
 }
